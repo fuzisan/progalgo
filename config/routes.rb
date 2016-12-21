9ae82200919a8ba240c347e53b3fa64d0a77256e
toshichan <Wals@example.com>
2016-12-21 14:53:32 +0900

    task1の引越し作業

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'home/top'
-  get 'home/task'
-  get 'home/task1ans'
-  get 'home/task1ans2'
-  get 'home/task1ans3'
+  get 'task1/task'
+  get 'task1/task1ans'
+  get 'task1/task1ans2'
+  get 'task1/task1ans3'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
