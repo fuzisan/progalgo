0d24c500a6db3b3f4bac1cacdcd0672b8d35b9d5
toshichan <Wals@example.com>
2016-12-21 16:14:11 +0900

    topページにリンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'task1/task1ans'
   get 'task1/task1ans2'
   get 'task1/task1ans3'
+  get 'task2/task2'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
