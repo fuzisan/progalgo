6a20506e5a54bf2cf24817f893d1db3fcf67c99a
toshichan <Wals@example.com>
2016-12-22 13:36:22 +0900

    川渡り問題のフレームを作成した

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'task1/task1ans2'
   get 'task1/task1ans3'
   get 'task2/task2'
+  get 'puzzle/fgb'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
