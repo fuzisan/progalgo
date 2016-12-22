ab7199692b74f38379d7bcc4fdd9b68ccc7d4cdf
toshichan <Wals@example.com>
2016-12-22 22:08:19 +0900

    application htmlからjsへjQueryコードを移動

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,3 +39,18 @@ function f_drop(event){
   //エラー回避のため、ドロップ処理の最後にdropイベントをキャンセルしておく
   event.preventDefault();
 }
+
+(function($){
+
+$(document).ready(function(){
+  var tablist = $("#tab_head li");
+  var tabbody = $("#tab_body li");
+
+  tablist.click(function(){
+    var idx = tablist.index($(this));
+    tablist.removeClass("on").eq(idx).addClass("on");
+    tabbody.removeClass("on").eq(idx).addClass("on");
+  });
+
+});
+})(jQuery);
