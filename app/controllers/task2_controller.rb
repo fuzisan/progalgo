1582d8456ecb96327f3094acc2762855431f22f8
toshichan <Wals@example.com>
2016-12-22 13:05:48 +0900

    解説コード追加

--- app/controllers/task2_controller.rb
+++ app/controllers/task2_controller.rb
@@ -1,2 +1,24 @@
 class Task2Controller < ApplicationController
+
+def task2ans
+@ans0=<<EOD
+for i in 1..100 do
+    ans = ""
+    if i%15 == 0
+        ans = "FizzBuzz"
+    elsif i%3 == 0
+        ans = "Fizz"
+    else i%5==0
+        ans == "Buzz"
+    end
+    puts "#{i}:#{ans}"
+end
+EOD
+@Commentary0 = <<EOD
+
+
+EOD
+end
+
+
 end
