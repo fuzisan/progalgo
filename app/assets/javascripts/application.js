5f8724ee8c958adb953a14eef13285de66a4bd9f
toshichan <Wals@example.com>
2016-12-25 19:12:44 +0900

    viewファイルにあったjsのうち移動できたものを移動した

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,7 @@
 //= require turbolinks
 //= require_tree .
 
-
+/*puzzle用*/
 /*参考引用元http://www.htmq.com/dnd/*/
 /***** ドラッグ開始時の処理 *****/
 function f_dragstart(event){
@@ -42,17 +42,135 @@ function f_drop(event){
   event.preventDefault();
 }
 
+/*task　解説，動画タブ*/
 (function($){
 
-$(document).ready(function(){
-  var tablist = $("#tab_head li");
-  var tabbody = $("#tab_body li");
+  $(document).ready(function(){
+    var tablist = $("#tab_head li");
+    var tabbody = $("#tab_body li");
 
-  tablist.click(function(){
-    var idx = tablist.index($(this));
-    tablist.removeClass("on").eq(idx).addClass("on");
-    tabbody.removeClass("on").eq(idx).addClass("on");
+    tablist.click(function(){
+      var idx = tablist.index($(this));
+      tablist.removeClass("on").eq(idx).addClass("on");
+      tabbody.removeClass("on").eq(idx).addClass("on");
+    });
+
+  });
+})(jQuery);
+
+/*task1ans2の解説アニメーション*/
+function task1ans2movie() {
+  var $deferredAnim = $.Deferred( function( deferredAnim ){
+
+    //thenでファンクション名を繋げる
+    deferredAnim.then(anim_01)
+    .then(anim_02)
+    .then(anim_03)
+    .then(anim_04)
+    .then(anim_05)
+    .then(anim_06)
+    .then(anim_07)
+    .then(anim_08)
+    .done( task1ans2movie )
+  });
+
+  //実行
+  $deferredAnim.resolve();
+
+  function anim_01 (){
+    return $("#line_5").delay(250).animate({
+      "background-color": "#87CEEB"
+    },1000),$("#ten").delay(250).animate({
+      "background-color": "#87CEEB"
+    },1000),$("#five").delay(250).animate({
+      "background-color": "#87CEEB"
+    },1000),$("#four").delay(250).animate({
+      "background-color": "#87CEEB"
+    },1000)
+  }
+  function anim_02 (){
+    return $("#line_5").delay(250).animate({
+      "background-color": "#778899"
+    },1000),$("#line_8").delay(250).animate({
+      "background-color": "#87CEEB"
+    },1000)
+
+  }
+  function anim_03 (){
+    return $("#line_8").delay(250).animate({
+      "background-color": "#778899"
+    },1000),$("#ten").delay(250).animate({
+      "background-color": "#87CEEB"
+    },1000)
+  }
+  function anim_04 (){
+    return $("#line_5").delay(250).animate({
+      "background-color": "#87CEEB"
+    },1000),$("#ten").delay(250).animate({
+      "background-color": "#ddd"
+    },1000),$("#three").delay(250).animate({
+      "background-color": "#87CEEB"
+    },1000)
+
+  }
+  function anim_05 (){
+    return $("#line_5").delay(250).animate({
+      "background-color": "#778899"
+    },1000),$("#line_6").delay(250).animate({
+      "background-color": "#87CEEB"
+    },1000),$("#circumference").delay(250).animate({
+      "background-color": "#87CEEB"
+    },1000),$("#line_7").delay(250).animate({
+      "background-color": "#87CEEB"
+    },1000)
+  }
+  function anim_06 (){
+    $("#circumference").text("12 circumference");
+    return $("#line_6").delay(250).animate({
+      "background-color": "#778899"
+    },1000),$("#line_7").delay(250).animate({
+      "background-color": "#778899"
+    },1000),$("#line_10").delay(250).animate({
+      "background-color": "#87CEEB"
+    },1000)
+  }
+
+  function anim_07(){
+    return $("#line_10").delay(250).animate({
+      "background-color": "#778899"
+    },1000),$("#five").delay(250).animate({
+      "background-color": "#ddd"
+    },1000),$("#four").delay(250).animate({
+      "background-color": "#ddd"
+    },1000),$("#three").delay(250).animate({
+      "background-color": "#ddd"
+    },1000),$("#circumference").delay(250).animate({
+      "background-color": "#ddd"
+    },1000)
+
+  }
+  function anim_08(){
+    $("#circumference").text("0 circumference");
+
+  }
+
+}
+
+
+<!--/*　ace editorをスクロールと一緒に動かす　*/-->
+function aceeditorscroll(){
+var initPosition = $(".code").offset().top;
+$(window).scroll(function() {
+  var scroll = $(document).scrollTop();
+
+  // 移動後ポジション
+  var movePosition = initPosition + scroll +500+ "px";
+  $(".code").animate({
+    top : movePosition
+  }, {
+    duration : 0,
+    queue : false
   });
 
 });
-})(jQuery);
+}
