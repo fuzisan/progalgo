d57360ccdcbc4977faa0918e28e4e05b57c8e002
toshichan <Wals@example.com>
2016-12-28 04:25:06 +0900

    task1ans1解説文言とコードの修正

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -6,23 +6,23 @@ def task1ans
   #ファイル参照にしたい
 @ans0 = <<EOD
 len = [2,3,4,5,10]
-ans = 0
+circumference = 0
 
-for i in 0..len.length do
-  for j in i+1..len.length do
-    for k in j+1..len.length do
-      element =[len[i],len[j],len[k]].map(&:to_i)
-      circumference = element.inject(:+)
+for i in 0..len.length-3 do
+  for j in i+1..len.length-2 do
+    for k in j+1..len.length-1 do
+      element = [len[i],len[j],len[k]].map(&:to_i)
+      sum = element.inject(:+)
       max = element.max
-      rest = circumference - max
+      rest = sum - max
       if max < rest
-          ans = ans > circumference ? ans : circumference
+          circumference = circumference < sum ? sum : circumference
       end
     end
   end
 end
 
-puts ans
+puts circumference
 EOD
 
 @Commentary0= <<EOD
@@ -45,7 +45,7 @@ EOD
     <select name="q3" required>
       <option value=""></option>
       <option value="false">10</option>
-      <option value="true">len.length</option>
+      <option value="true">len.length-3</option>
       <option value="false">100</option>
     </select>まで</p>
     <p>jは<select name="q4" required>
@@ -58,7 +58,7 @@ EOD
     <select name="q5" required>
       <option value=""></option>
       <option value="false">10</option>
-      <option value="true">len.length</option>
+      <option value="true">len.length-2</option>
       <option value="false">100</option>
     </select>まで</p>
     <p>kは<select name="q6" required>
@@ -71,7 +71,7 @@ EOD
     <select name="q7" required>
       <option value=""></option>
       <option value="false">10</option>
-      <option value="true">len.length</option>
+      <option value="true">len.length-1</option>
       <option value="false">100</option>
     </select>まで回っている．</p>
 
@@ -105,23 +105,23 @@ EOD
       <option value="true">3</option>
     </select>つの数字の選んでいることがわかる</p>
     <h3>繰り返されている処理を読む</h3>
-    <p>変数circumferenceに選ばれた数字の<select name="q13" required>
+    <p>変数sumに選ばれた数字の<select name="q13" required>
       <option value=""></option>
       <option value="false">最大値</option>
       <option value="true">合計値</option>
-      <option value="false">変数circumferenceと変数maxの差分</option>
+      <option value="false">変数sumと変数maxの差分</option>
     </select>が入っている</p>
     <p>  変数maxに選ばれた数字の<select name="q14" required>
       <option value=""></option>
       <option value="true">最大値</option>
       <option value="false">合計値</option>
-      <option value="false">変数circumferenceと変数maxの差分</option>
+      <option value="false">変数sumと変数maxの差分</option>
     </select>が入っている</p>
     <p>  変数restに<select name="q15" required>
       <option value=""></option>
       <option value="false">最大値</option>
       <option value="false">合計値</option>
-      <option value="true">変数circumferenceと変数maxの差分</option>
+      <option value="true">変数sumと変数maxの差分</option>
     </select>が入っている</p>
     <h3>if文の条件とされている式を読む</h3>
     <p>このif文の条件は，変数maxが変数restよりも<select name="q16" required>
@@ -130,7 +130,7 @@ EOD
       <option value="false">大きい</option>
     </select>とき</p>
     <h3>if文の中身を読む</h3>
-    <p>変数ansが，現在の値よりも変数cicumferenceが大きいときは現在値のまま，小さいときは変数circumferenceを代入している．</p>
+    <p>変数circumferenceが，現在の値よりも変数sumが大きいときは変数sumの値を，小さいときは変数circumferenceの値を代入している．</p>
     <h3>つまりこのプログラムは</h3>
     <p>配列から重複を<select name="q17" required>
       <option value=""></option>
