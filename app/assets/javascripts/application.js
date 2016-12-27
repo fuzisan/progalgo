7964c6d5c271b2ead662994a124677a2a7171b30
toshichan <Wals@example.com>
2016-12-28 04:24:36 +0900

    task1ansの解説アニメーションの作成

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -58,6 +58,251 @@ function f_drop(event){
   });
 })(jQuery);
 
+/*task1ans1の解説アニメーション*/
+function task1ans1movie() {
+  var $deferredAnim = $.Deferred( function( deferredAnim ){
+
+    //thenでファンクション名を繋げる
+    deferredAnim.then(anim_234)
+    .then(anim_234text)
+    .then(anim_235)
+    .then(anim_235text)
+    .then(anim_2310)
+    .then(anim_2310text)
+    .then(anim_245)
+    .then(anim_245text)
+    .then(anim_2410)
+    .then(anim_2410text)
+    .then(anim_2510)
+    .then(anim_2510text)
+    .then(anim_345)
+    .then(anim_345text)
+    .then(anim_3410)
+    .then(anim_3410text)
+    .then(anim_3510)
+    .then(anim_3510text)
+    .then(anim_4510)
+    .then(anim_4510text)
+    .then(finish)
+    .then(anim_12)
+    .then(anim_12text)
+    .done(task1ans1movie)
+  });
+
+  //実行
+  $deferredAnim.resolve();
+  /*2,3,4*/
+  function anim_234 (){
+    return $("#two").delay(250).animate({
+      "background-color": "red"
+    },1000),$("#three").delay(250).animate({
+      "background-color": "blue"
+    },1000),$("#four").delay(250).animate({
+      "background-color": "green"
+    },1000)
+  }
+
+  function anim_234text(){
+    $("#circumference").text("9 circumference");
+    $("#sum").text("9 sum");
+    $("#max").text("4 max");
+    $("#rest").text("5 rest");
+    $("#maxrest").text("true max<rest");
+  }
+
+  /*2,3,5*/
+  function anim_235 (){
+    return $("#four").delay(250).animate({
+      "background-color": "#ddd"
+    },1000),$("#five").delay(250).animate({
+      "background-color": "green"
+    },1000)
+  }
+
+  function anim_235text(){
+    $("#circumference").text("9 circumference");
+    $("#sum").text("10 sum");
+    $("#max").text("5 max");
+    $("#rest").text("5 rest");
+    $("#maxrest").text("false max<rest");
+  }
+
+  /*2,3,10*/
+  function anim_2310 (){
+    return $("#five").delay(250).animate({
+      "background-color": "#ddd"
+    },1000),$("#ten").delay(250).animate({
+      "background-color": "green"
+    },1000)
+  }
+
+  function anim_2310text(){
+    $("#circumference").text("9 circumference");
+    $("#sum").text("15 sum");
+    $("#max").text("10 max");
+    $("#rest").text("5 rest");
+    $("#maxrest").text("false max<rest");
+  }
+
+  /*2,4,5*/
+  function anim_245 (){
+    return $("#three").delay(250).animate({
+      "background-color": "#ddd"
+    },1000),$("#ten").delay(250).animate({
+      "background-color": "#ddd"
+    },1000),$("#four").delay(250).animate({
+      "background-color": "blue"
+    },1000),$("#five").delay(250).animate({
+      "background-color": "green"
+    },1000)
+  }
+
+  function anim_245text(){
+    $("#circumference").text("11 circumference");
+    $("#sum").text("11 sum");
+    $("#max").text("5 max");
+    $("#rest").text("6 rest");
+    $("#maxrest").text("true max<rest");
+  }
+
+  /*2,4,10*/
+  function anim_2410 (){
+    return $("#five").delay(250).animate({
+      "background-color": "#ddd"
+    },1000),$("#ten").delay(250).animate({
+      "background-color": "green"
+    },1000)
+  }
+
+  function anim_2410text(){
+    $("#circumference").text("11 circumference");
+    $("#sum").text("16 sum");
+    $("#max").text("10 max");
+    $("#rest").text("6 rest");
+    $("#maxrest").text("false max<rest");
+  }
+
+  /*2,5,10*/
+  function anim_2510 (){
+    return $("#four").delay(250).animate({
+      "background-color": "#ddd"
+    },1000),$("#five").delay(250).animate({
+      "background-color": "blue"
+    },1000),$("#ten").delay(250).animate({
+      "background-color": "green"
+    },1000)
+  }
+
+  function anim_2510text(){
+    $("#circumference").text("11 circumference");
+    $("#sum").text("17 sum");
+    $("#max").text("10 max");
+    $("#rest").text("7 rest");
+    $("#maxrest").text("false max<rest");
+  }
+
+  /*3,4,5*/
+  function anim_345(){
+    return $("#two").delay(250).animate({
+      "background-color": "#ddd"
+    },1000),$("#ten").delay(250).animate({
+      "background-color": "#ddd"
+    },1000),$("#three").delay(250).animate({
+      "background-color": "red"
+    },1000),$("#four").delay(250).animate({
+      "background-color": "blue"
+    },1000),$("#five").delay(250).animate({
+      "background-color": "green"
+    },1000)
+  }
+
+  function anim_345text(){
+    $("#circumference").text("12 circumference");
+    $("#sum").text("12 sum");
+    $("#max").text("5 max");
+    $("#rest").text("7 rest");
+    $("#maxrest").text("true max<rest");
+  }
+
+  /*3,4,10*/
+  function anim_3410(){
+    return $("#five").delay(250).animate({
+      "background-color": "#ddd"
+    },1000),$("#ten").delay(250).animate({
+      "background-color": "green"
+    },1000)
+
+  }
+
+  function anim_3410text(){
+    $("#circumference").text("12 circumference");
+    $("#sum").text("17 sum");
+    $("#max").text("10 max");
+    $("#rest").text("7 rest");
+    $("#maxrest").text("false max<rest");
+  }
+
+  /*3,5,10*/
+  function anim_3510(){
+    return $("#four").delay(250).animate({
+      "background-color": "#ddd"
+    },1000),$("#five").delay(250).animate({
+      "background-color": "blue"
+    },1000)
+  }
+
+  function anim_3510text(){
+    $("#circumference").text("12 circumference");
+    $("#sum").text("18 sum");
+    $("#max").text("10 max");
+    $("#rest").text("8 rest");
+    $("#maxrest").text("false max<rest");
+  }
+
+  /*4,5,10*/
+  function anim_4510(){
+    return $("#three").delay(250).animate({
+      "background-color": "#ddd"
+    },1000),$("#four").delay(250).animate({
+      "background-color": "red"
+    },1000)
+  }
+
+  function anim_4510text(){
+    $("#circumference").text("12 circumference");
+    $("#sum").text("19 sum");
+    $("#max").text("10 max");
+    $("#rest").text("9 rest");
+    $("#maxrest").text("false max<rest");
+  }
+
+  function finish(){
+    return $("#circumference").delay(250).animate({
+      "background-color": "red"
+    },1000)
+  }
+
+  function anim_12(){
+    return $("#four").delay(250).animate({
+      "background-color": "#ddd"
+    },1000),$("#five").delay(250).animate({
+      "background-color": "#ddd"
+    },1000),$("#ten").delay(250).animate({
+      "background-color": "#ddd"
+    },1000),$("#circumference").delay(250).animate({
+      "background-color": "#ddd"
+    },1000)
+  }
+
+  function anim_12text(){
+    $("#circumference").text("0 circumference");
+    $("#sum").text("sum");
+    $("#max").text("max");
+    $("#rest").text("rest");
+    $("#maxrest").text("false max<rest");
+  }
+}
+
 /*task1ans2の解説アニメーション*/
 function task1ans2movie() {
   var $deferredAnim = $.Deferred( function( deferredAnim ){
