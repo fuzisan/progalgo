6c7ed7b4fa51f41bbff7c5993e25ec2e9e8b2a27
toshichan <Wals@example.com>
2016-12-28 11:50:17 +0900

    taskのindexページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'home#top'
   get 'ruby_gatebook' =>'home#ruby_gatebook'
   get 'way_of_thinking' => 'home#way_of_thinking'
+  get 'task/index'
   get 'task/task'
   get 'task/task1ans'
   get 'task/task1ans2'
