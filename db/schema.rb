baac6a9aa835538ba9125bbc678c4b1bd2604623
toshichan <Wals@example.com>
2017-01-22 22:13:52 +0900

    tasks db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170122055343) do
+ActiveRecord::Schema.define(version: 20170122112420) do
 
   create_table "readings", force: :cascade do |t|
     t.string   "title"
     t.text     "code"
     t.text     "animation"
+    t.text     "question"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title"
     t.text     "question"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
